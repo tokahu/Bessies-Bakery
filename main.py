66ee8617948a0fbfb8d005d15cf3667a178838e9
tokahu <145630633+tokahu@users.noreply.github.com>
2024-06-10 00:58:29 -0400

    6/9
    I got the egg to finally crack into the bowl (although sometimes it still doesn't work) and I added a "coming soon" sign at the end of the game to signal that the game ends there for now.

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from forwardarrowfile import ForwardArrow
 from backwardsarrowfile import BackArrow
 from speechbubblefile import SpeechBubble
 from wholeeggfile import WholeEgg
+from crackedeggfile import CrackedEgg
+from comingsoonfile import ComingSoon
 
 pygame.init()
 pygame.font.init()
@@ -44,11 +46,13 @@ music_button = MusicButton(20, 20)
 no_music_button = NoMusicButton(20,20)
 mixing_bowl = Bowl(300, 250)
 butter = Butter(700, 300)
-meltedbutter = MeltedButter(500,250)
+meltedbutter = MeltedButter(520,250)
 forwardarrow = ForwardArrow(895, 490)
 backarrow = BackArrow(20,490)
 bubble = SpeechBubble(150, 100)
-wholeegg = WholeEgg(230, 80)
+wholeegg = WholeEgg(700, 300)
+crackedegg = CrackedEgg(490, 200)
+comingsoon = ComingSoon(400, 100)
 
 music = pygame.mixer.music.load('BakerySoundtrack.mp3')
 pygame.mixer.music.play(-1)
@@ -72,80 +76,84 @@ music = True
 draggingbutter = False
 draggingegg = False
 butterin = False
+eggin = False
 
 # Main game loop
 
 running = True
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONUP:
-            if play_button.rect.collidepoint(event.pos) and scene == 0:
+        elif event.type == pygame.MOUSEBUTTONUP:
+            draggingbutter = False
+            draggingegg = False
+            if scene == 0 and play_button.rect.collidepoint(event.pos):
                 scene += 1
-            if forwardarrow.rect.collidepoint(event.pos):
+            elif forwardarrow.rect.collidepoint(event.pos):
                 scene += 1
-            if backarrow.rect.collidepoint(event.pos):
+                if scene >= 3:
+                    scene = 3
+            elif backarrow.rect.collidepoint(event.pos):
                 scene -= 1
-            if back_button.rect.collidepoint(event.pos):
+                if scene <= 0:
+                    scene = 0
+            elif back_button.rect.collidepoint(event.pos):
                 scene = 0
-            if music_button.rect.collidepoint(event.pos):
+            elif music_button.rect.collidepoint(event.pos):
                 music = not music
-            if butter.rect.collidepoint((500,250)):
+            elif butter.rect.collidepoint(event.pos):
                 butterin = True
-            elif event.button == 1:
-                dragging = False
+            elif wholeegg.rect.collidepoint(event.pos):
+                eggin = True  # Set eggin to True when the egg is added
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if butter.rect.collidepoint(event.pos):
                     draggingbutter = True
-                if wholeegg.rect.collidepoint(event.pos):
+                elif wholeegg.rect.collidepoint(event.pos):
                     draggingegg = True
         elif event.type == pygame.MOUSEMOTION:
             if draggingbutter:
                 butter.rect.move_ip(event.rel)
-            if draggingegg:
+            elif draggingegg:
                 wholeegg.rect.move_ip(event.rel)
 
+    # Scene rendering
+    screen.fill((176, 139, 120))
     if scene == 0:
-        screen.fill((0, 0, 0))
         screen.blit(bg_image.image, (0, 0))
         screen.blit(play_button.image, (400, 250))
     elif scene == 1:
-        screen.fill((0, 0, 0))
         screen.blit(counter_screen.image, (0, 0))
-        screen.blit(back_button.image, (895, 20))
-        screen.blit(music_button.image, (20, 20))
-        screen.blit(forwardarrow.image,(895, 490))
-        screen.blit(backarrow.image, (20, 490))
-        screen.blit(bubble.image, (150,100))
+        screen.blit(bubble.image, (150, 100))
         screen.blit(display_topping_text, (195, 200))
     elif scene == 2:
-        screen.fill((0,0,0))
         screen.blit(close_counter_screen.image, (0, 0))
-        screen.blit(back_button.image, (895, 20))
-        screen.blit(forwardarrow.image,(895, 490))
-        screen.blit(backarrow.image, (20, 490))
-        screen.blit(music_button.image, (20, 20))
         screen.blit(mixing_bowl.image, (230, 50))
         if not butterin:
             screen.blit(butter.image, butter.rect)
-        elif butterin:
-            screen.blit(meltedbutter.image, (230, 80))
-        screen.blit(wholeegg.image, (230, 80))
+        else:
+            screen.blit(meltedbutter.image, (250, 90))
+        if not eggin and butterin:
+            screen.blit(wholeegg.image, wholeegg.rect)
+        elif eggin:
+            screen.blit(crackedegg.image, (310, 110))
     elif scene == 3:
-        screen.fill((0,0,0))
+        screen.fill((176, 139, 120))
+        screen.blit(comingsoon.image, (250, 50))
+    if scene != 0:
+        screen.blit(back_button.image, (895, 20))
         screen.blit(forwardarrow.image, (895, 490))
         screen.blit(backarrow.image, (20, 490))
-        screen.blit(back_button.image, (895, 20))
+        screen.blit(music_button.image, (20, 20))
 
-    if not music:
-        pygame.mixer.music.pause()
-        screen.blit(no_music_button.image, (20, 20))
+    # Music control
     if music:
         pygame.mixer.music.unpause()
         screen.blit(music_button.image, (20, 20))
-
+    else:
+        pygame.mixer.music.pause()
+        screen.blit(no_music_button.image, (20, 20))
 
     pygame.display.update()
+pygame.quit()
